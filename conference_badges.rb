30d0dd100a43c3bf06b2346d27a70ca289f09c9d
Matt Mason <masonmj10@gmail.com>
2020-11-17 19:58:34 -0500

    commit tree 94a4dc9ea3f8587e3e5e966feab627758385a09d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+
+
+
+def badge_maker(name)
+"Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+    attendees.collect do |name|
+        badge_maker(name)
+    end 
+end 
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each_with_index do |attendees, room| 
+       room_assignments << "Hello, #{attendees}! You'll be assigned to room #{room +1}!" 
+       
+    end 
+    room_assignments
+end 
+
+def printer(attendees)
+    batch_badge_creator(attendees).map do |attendee|
+        puts attendee
+    end 
+    assign_rooms(attendees).map do |attendee| 
+        puts attendee
+    end 
+end
